bcf8da8c7cf77bcd9f4893ecc230cbf0d3d1ea47
Vikrant Mantalwad <vmantalwad@gmail.com>
2025-03-22 16:18:40 +0530

    commit from command prompt

--- js/app.js
+++ js/app.js
@@ -1,12 +1,11 @@
-const date = (document.getElementById('date').innerHTML =
-  new Date().getFullYear())
+const date = (document.getElementById("date").innerHTML =
+  new Date().getFullYear());
 
+console.log("Hii");
 
-const navBtn = document.getElementById('nav-toggle')
-const links = document.getElementById('nav-links')
+const navBtn = document.getElementById("nav-toggle");
+const links = document.getElementById("nav-links");
 
-navBtn.addEventListener('click', () => {
-  links.classList.toggle('show-links')
-})
-
-
+navBtn.addEventListener("click", () => {
+  links.classList.toggle("show-links");
+});
